11e64bee720ad761a5820a11422d1c478975f293
Nadya <nadbelyh@yandex.ru>
2023-02-07 12:58:35 +0500

    Домашнее задание к лекции «Object, Reflection и Proxy»

---
+++ src/js/destructuring.js
@@ -0,0 +1,15 @@
+export default function specialAttack({ special }) {
+  const resultArr = [];
+
+  special.forEach((item) => {
+    const { id, name, icon, description = 'Описание недоступно' } = item;
+    resultArr.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  });
+
+  return resultArr;
+}
